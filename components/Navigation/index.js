5440e6a7c471f010bd0a8129faad37e67c1604b3
Niko Kreissl <nikokreissl@gmail.com>
2023-03-12 12:15:30 +0100

    Center main content

---
+++ components/Navigation/index.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export default function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,13 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import Card from "@/components/Card";
+import Main from "@/components/Main";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <>
+    <Main>
       <Card />
-    </>
+    </Main>
   );
 }
