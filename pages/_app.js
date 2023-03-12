e194e7a3f0a9cc2223388af6e1198be131d7b70d
Niko Kreissl <nikokreissl@gmail.com>
2023-03-12 12:46:04 +0100

    Install SWR and fetch data

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
 import "@/styles/globals.css";
 import Header from "@/components/Header";
 import Navigation from "@/components/Navigation";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
+  const { data, error } = useSWR(
+    "https://rickandmortyapi.com/api/character",
+    fetcher
+  );
+
+  console.log(data);
+
+  if (error) {
+    console.log(error);
+  }
   return (
     <>
       <Header />
