eeb68e573c0c02d2037bdf1ffc07195bc24d1afd
Niko Kreissl <nikokreissl@gmail.com>
2023-03-12 12:35:34 +0100

    Add navigation

--- components/Navigation/index.js
+++ components/Navigation/index.js
@@ -1,14 +1,48 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
+import Link from "next/link";
 
 export default function Navigation() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-      </ul>
-    </nav>
+    <StyledNavigation>
+      <StyledNavigationList>
+        <StyledNavigationItem>
+          <StyledNavigationLink href={"/"}>Home</StyledNavigationLink>
+        </StyledNavigationItem>
+        <StyledNavigationItem>
+          <StyledNavigationLink href={"/"}>Link 1</StyledNavigationLink>
+        </StyledNavigationItem>
+        <StyledNavigationItem>
+          <StyledNavigationLink href={"/"}>Link 2</StyledNavigationLink>
+        </StyledNavigationItem>
+        <StyledNavigationItem>
+          <StyledNavigationLink href={"/"}>Link 3</StyledNavigationLink>
+        </StyledNavigationItem>
+      </StyledNavigationList>
+    </StyledNavigation>
   );
 }
+
+const StyledNavigation = styled.nav``;
+
+const StyledNavigationList = styled.ul`
+  display: flex;
+  justify-content: space-between;
+  list-style: none;
+  height: 70px;
+  background-color: var(--primary-background-color);
+`;
+
+const StyledNavigationItem = styled.li`
+  flex: 1;
+  text-align: center;
+`;
+
+const StyledNavigationLink = styled(Link)`
+  display: block;
+  width: 100%;
+  height: 100%;
+  line-height: 70px;
+  &:hover {
+    background-color: var(--secondary-background-color);
+  }
+`;
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import Header from "@/components/Header";
+import Navigation from "@/components/Navigation";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <Navigation />
     </>
   );
 }
